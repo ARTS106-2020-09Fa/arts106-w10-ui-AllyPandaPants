c4dda972fa63891b5c5a50f674586c91ac0e6471
AllyPandaPants <allydoc99@gmail.com>
2020-11-10 14:13:50 -0500

    Part-3

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -9,6 +9,8 @@ public class UIManager : MonoBehaviour
     public Animator startButton;
     public Animator settingsButton;
     public Animator dialog;
+    public Animator contentPanel;
+    public Animator gearImage;
 
     public void OpenSettings()
     {
@@ -25,5 +27,11 @@ public class UIManager : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene("RocketMouse");
+    }
+    public void ToggleMenu()
+    {
+        bool isHidden = contentPanel.GetBool("isHidden");
+        contentPanel.SetBool("isHidden", !isHidden);
+        gearImage.SetBool("isHidden", !isHidden);
     }
 }
